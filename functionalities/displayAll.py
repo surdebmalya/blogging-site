50ef64b7858e473eadddd80b2ad1a6f50e26a77a
Arshia-Chaudhuri <arshiachaudhuri99@gmail.com>
2024-02-24 23:49:39 +0530

    updated

---
+++ functionalities/displayAll.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+import json
+from utils.db import db, Blog, AllBlogs
+
+def display_all():
+    blogs = AllBlogs.query.all()
+
+    result=[]
+    for blog in blogs:
+        userName=blog.userName
+        blogId=blog.blogId
+        blogData=Blog.query.get(blogId)
+        blogTitle=blogData.title
+        blogBody=blogData.body
+        blogImageURI=blogData.imageURI
+        post={}
+        post["userName"] = userName
+        post["title"] = blogTitle
+        post["body"] = blogBody
+        post["imageURI"] = blogImageURI
+        result.append(post)
+    
+    result = result[::-1]
+    return result
